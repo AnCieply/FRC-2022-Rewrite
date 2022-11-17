ab5a6724024119a29664809665ae815ea0388185
Igor Cieply <igorcieply@gmail.com>
2022-11-17 17:25:08 -0600

    modified drivetrain subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,10 +13,33 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public final static class Motor {
+    public final static class Drive {
+        // CAN ids
         public static final int kFrontLeftMotorID = 3;
         public static final int kBackLeftMotorID = 4;
         public static final int kFrontRightMotorID = 1;
         public static final int kBackRightMotorID = 2;
+
+        // Direction constants (NEW)
+        public static final boolean kLeftInverted = false;
+        public static final boolean kRightInverted = true;
+        public static final boolean kGyroReversed = true;
+        public static final boolean kRotationReversed = false;
+
+        // Physical robot properties (NEW)
+        public static final double kTrackWidth = 22;
+        public static final double kWheelRadius = 3;
+        public static final double kGearRatio = 10.71;
+        public static final double kEncoderResolution = 2048;
+
+        // Feed forward values.
+        public static final double kS = 0;
+        public static final double kV = 0;
+        public static final double kA = 0;
+
+        // PID values.
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
     }
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -6,10 +6,17 @@ package frc.robot.subsystems;
 
 import static frc.robot.Constants.*;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrain extends SubsystemBase {
@@ -22,14 +29,40 @@ public class DriveTrain extends SubsystemBase {
     private boolean gyroReversed;
     private boolean isReversed;
 
+    private DifferentialDrive driveTrain;
+    private DifferentialDriveOdometry driveTrainOdo;
+    private DifferentialDriveKinematics driveTrainKinema;
+
+    private SimpleMotorFeedforward feedForward;
+
+    private double velocity;
+    private double rotation;
 
     public DriveTrain() {
-        frontLeftMotor = new WPI_TalonFX(Motor.kFrontLeftMotorID);
-        frontRightMotor = new WPI_TalonFX(Motor.kFrontRightMotorID);
-        backLeftMotor = new WPI_TalonFX(Motor.kBackLeftMotorID);
-        backRightMotor = new WPI_TalonFX(Motor.kBackRightMotorID);
+        frontLeftMotor = new WPI_TalonFX(Drive.kFrontLeftMotorID);
+        frontRightMotor = new WPI_TalonFX(Drive.kFrontRightMotorID);
+        backLeftMotor = new WPI_TalonFX(Drive.kBackLeftMotorID);
+        backRightMotor = new WPI_TalonFX(Drive.kBackRightMotorID);
         
+        driveTrain = new DifferentialDrive(frontLeftMotor, frontRightMotor);
+        driveTrain.setSafetyEnabled(false);
 
+        feedForward = new SimpleMotorFeedforward(Drive.kS, Drive.kV, Drive.kA);
+        leftPIDController = new PIDController(Drive.kP, Drive.kI, Drive.kD);
+        rightPIDController = new PIDController(Drive.kP, Drive.kI, Drive.kD);
+
+        setFactory(frontLeftMotor);
+        setFactory(backLeftMotor);
+        setFactory(backRightMotor);
+        setFactory(frontRightMotor);
+
+        frontLeftMotor.setInverted(Drive.kLeftInverted);
+        backLeftMotor.setInverted(Drive.kLeftInverted);
+        frontRightMotor.setInverted(Drive.kRightInverted);
+        backRightMotor.setInverted(Drive.kRightInverted);
+
+        backLeftMotor.follow(frontLeftMotor);
+        backRightMotor.follow(frontRightMotor);
     }
 
     @Override
@@ -40,5 +73,83 @@ public class DriveTrain extends SubsystemBase {
     @Override
     public void simulationPeriodic() {
         // This method will be called once per scheduler run during simulation
+    }
+
+    public void setFactory(WPI_TalonFX motor) {
+        motor.configFactoryDefault();
+    }
+
+    public void setDriveMode(NeutralMode driveMode) {
+        frontLeftMotor.setNeutralMode(driveMode);
+        frontRightMotor.setNeutralMode(driveMode);
+        backLeftMotor.setNeutralMode(driveMode);
+        backRightMotor.setNeutralMode(driveMode);
+    }
+
+    public void setVolts(double leftVoltage, double rightVoltage) {
+        frontLeftMotor.setVoltage(leftVoltage);
+        frontRightMotor.setVoltage(rightVoltage);
+        driveTrain.feed();
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        driveTrain.setMaxOutput(maxSpeed);
+    }
+
+    public void setApplyArcadeDrive(double velocity, double rotation) {
+        this.velocity = velocity;
+        this.rotation = rotation;
+        driveTrain.arcadeDrive(velocity, rotation);
+    }
+
+    public double getArcadeVelocity() {
+        return velocity;
+    }
+
+    public double getArcadeRotation() {
+        return rotation;
+    }
+
+    public double getLeftWheelDistance() {
+        double leftDistance = ((double) getLeftEncoderPosition()) / Drive.kEncoderResolution / Drive.kGearRatio * 2 * Math.PI * Units.inchesToMeters(Drive.kWheelRadius);
+        return leftDistance;
+    }
+
+    public double getRightWheelDistance() {
+        double rightDistance = ((double) getRightEncoderPosition()) / Drive.kEncoderResolution / Drive.kGearRatio * 2 * Math.PI * Units.inchesToMeters(Drive.kWheelRadius);
+        return rightDistance;
+    }
+
+    public double getLeftEncoderPosition() {
+        return frontLeftMotor.getSelectedSensorPosition();
+    }
+    
+    public double getRightEncoderPosition() {
+        return frontRightMotor.getSelectedSensorPosition();
+    }
+    
+    public double getLeftEncoderVelocity() {
+        return frontLeftMotor.getSelectedSensorVelocity();
+    }
+    
+    public double getRightEncoderVelocity() {
+        return frontRightMotor.getSelectedSensorVelocity();
+    }
+
+    public SimpleMotorFeedforward getSimpleMotorFeedForward() {
+        return feedForward;
+    }
+    
+    public PIDController getLeftPIDController() {
+        return leftPIDController;
+    }
+    
+    public PIDController getRightPIDController() {
+        return rightPIDController;
+    }
+
+    public void resetEncoders() {
+        frontLeftMotor.setSelectedSensorPosition(0);
+        frontRightMotor.setSelectedSensorPosition(0);
     }
 }
